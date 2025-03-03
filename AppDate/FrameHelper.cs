db2417dba3fd7caca2d09ab94075e80b827c906c
Ivan Krysenko <assuss2120@gmail.com>
2025-03-04 00:33:21 +0300

    Всё плохо

--- AppDate/FrameHelper.cs
+++ AppDate/FrameHelper.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace TestWork.AppDate
 {
     internal class FrameHelper
     {
         public static Frame mainFrame = new Frame();
+        public static Frame secondFrame = new Frame();
     }
 }
--- View/PageTeacher.xaml.cs
+++ View/PageTeacher.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TestWork.AppDate;
 
 namespace TestWork.View
@@ -24,16 +12,18 @@ namespace TestWork.View
         public PageTeacher()
         {
             InitializeComponent();
+            FrameHelper.secondFrame = SecondFrame;
         }
 
         private void AddStudent_Click(object sender, RoutedEventArgs e)
         {
-           // FrameHelper.mainFrame(new View.PageAddStudent()); // спросите у Монастыревой, как сделать ссылку на страницу в странице. Просто мы хз
+            SecondFrame.Navigate(new View.PageAddGroup());
+
         }
 
         private void AddGroupBtn_Click(object sender, RoutedEventArgs e)
         {
-           // FrameHelper.mainFrame(new View.PageAddGroup()); // спросите у Монастыревой, как сделать ссылку на страницу в странице. Просто мы хз
+            SecondFrame.Navigate(new View.PageAddStudent());
         }
 
         private void StudentMarkBtn_Click(object sender, RoutedEventArgs e)
